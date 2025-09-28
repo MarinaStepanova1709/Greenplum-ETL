4e5164db7aa67e40a336dfc3b4bff98df0b3e918
MarinaStepanova1709 <73593065+MarinaStepanova1709@users.noreply.github.com>
2025-09-28 20:48:20 +0300

    Update and rename CH_create_ext.sql to ch_create_distr.sql

--- Clickhouse/ch_create_distr.sql
+++ Clickhouse/ch_create_distr.sql
@@ -18,7 +18,7 @@ CREATE TABLE std12_116_pr.sales_report_daily_20210101_20210301  (
 	"СреднЧек"  Decimal(19,4),
 	"СрВыручкНаПокупателя" Decimal(19,4)
 )
-Engine = PostgreSQL('192.168.214.203:5432', 'adb', 'sales_report_daily_20210101_20210301', 'std12_116', '***', 'std12_116_pr');
+Engine = PostgreSQL('192.168.214.203:5432', 'adb', 'sales_report_daily_20210101_20210301', 'std12_116', 'M8oKCODdpha1K4', 'std12_116_pr');
 order by  dt
 
 
@@ -41,8 +41,32 @@ CREATE TABLE std12_116_pr.sales_report_monthly_20210101_20210301  (
 	"СреднЧек"  Decimal(19,4),
 	"СрВыручкНаПокупателя" Decimal(19,4)
 )
-Engine = PostgreSQL('192.168.214.203:5432', 'adb', 'sales_report_monthly_20210101_20210301', 'std12_116', '***', 'std12_116_pr');
+Engine = PostgreSQL('192.168.214.203:5432', 'adb', 'sales_report_monthly_20210101_20210301', 'std12_116', 'M8oKCODdpha1K4', 'std12_116_pr');
 order by  dt
 
 
+CREATE TABLE std12_116_pr.sales_report_daily_20210101_20210301
+ON CLUSTER default_cluster
+(
+    plant String,
+    dt Date,
+    revenue Decimal(18,2), 
+    sum_disc Decimal(18,2),
+    sales_wo_discount Decimal(18,2),
+    sum_goods Int32,
+    qty_bills Int32, 
+    sum_traf Int32,
+    cnt_coupons Int32,
+    "доляТоваровПоАкции" Decimal(18,2),
+    "среднКолТовВЧеке" Decimal(18,2),
+    "КонверсМагазина" Decimal(18,2),
+    "СреднЧек" Decimal(19,4),
+    "СрВыручкНаПокупателя" Decimal(19,4)
+)
+ENGINE = ReplicatedMergeTree(
+    '/click/ch_sales_12_116/{shard}',
+    '{replica}'                               -- имя реплики
+)
+ORDER BY ( plant, dt)
+SETTINGS index_granularity = 8192;
 
